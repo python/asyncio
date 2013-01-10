f87f5fb6e4cc1c8218e5a1ae16090263d48c4939
Guido van Rossum <guido@python.org>
2013-01-10 09:57:35 -0800

    Add test id pattern matching feature to runtests.py.

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 PYTHON=python3
 COVERAGE=coverage3
 NONTESTS=`find tulip -name [a-z]\*.py ! -name \*_test.py`
+FLAGS=
 
 test:
-	$(PYTHON) runtests.py -v
+	$(PYTHON) runtests.py -v $(FLAGS)
 
 testloop:
-	while sleep 1; do $(PYTHON) runtests.py -v; done
+	while sleep 1; do $(PYTHON) runtests.py -v $(FLAGS); done
 
 cov coverage:
-	$(COVERAGE) run runtests.py -v
+	$(COVERAGE) run runtests.py -v $(FLAGS)
 	$(COVERAGE) html $(NONTESTS)
 	$(COVERAGE) report -m $(NONTESTS)
 	echo "open file://`pwd`/htmlcov/index.html"
--- runtests.py
+++ runtests.py
@@ -1,12 +1,27 @@
-"""Run all unittests."""
+"""Run all unittests.
+
+Usage:
+  python3 runtests.py [-v] [-q] [pattern] ...
+
+Where:
+  -v: verbose
+  -q: quiet
+  pattern: optional regex patterns to match test ids (default all tests)
+
+Note that the test id is the fully qualified name of the test,
+including package, module, class and method,
+e.g. 'tulip.events_test.PolicyTests.testPolicy'.
+"""
 
 # Originally written by Beech Horn (for NDB).
 
+import re
 import sys
 import unittest
 
+assert sys.version.startswith('3'), 'Please use Python 3.3 or higher.'
 
-def load_tests():
+def load_tests(patterns=()):
   mods = ['events', 'futures', 'tasks']
   test_mods = ['%s_test' % name for name in mods]
   tulip = __import__('tulip', fromlist=test_mods)
@@ -19,19 +34,26 @@ def load_tests():
       if name.endswith('Tests'):
         test_module = getattr(mod, name)
         tests = loader.loadTestsFromTestCase(test_module)
+        if patterns:
+          tests = [test
+                   for test in tests
+                   if any(re.search(pat, test.id()) for pat in patterns)]
         suite.addTests(tests)
 
   return suite
 
 
 def main():
+  patterns = []
   v = 1
   for arg in sys.argv[1:]:
     if arg.startswith('-v'):
       v += arg.count('v')
     elif arg == '-q':
       v = 0
-  result = unittest.TextTestRunner(verbosity=v).run(load_tests())
+    elif arg and not arg.startswith('-'):
+      patterns.append(arg)
+  result = unittest.TextTestRunner(verbosity=v).run(load_tests(patterns))
   sys.exit(not result.wasSuccessful())
 
 

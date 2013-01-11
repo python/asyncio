00209220ccf8ecfdc62186a1103891b2807c3854
Guido van Rossum <guido@python.org>
2013-01-11 15:15:33 -0800

    Use more lenient decoding practice.

--- curl.py
+++ curl.py
@@ -23,8 +23,8 @@ def main():
     for k, v in headers.items():
         print('{}: {}'.format(k, v))
     print()
-    data = p.event_loop.run_until_complete(tulip.Task(stream.read(1000)))
-    print(data.decode('utf-8'))
+    data = p.event_loop.run_until_complete(tulip.Task(stream.read(1000000)))
+    print(data.decode('utf-8', 'replace'))
 
 
 if __name__ == '__main__':

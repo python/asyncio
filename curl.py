016e04a62095be63f8c36929c6f6b5e78930892f
Guido van Rossum <guido@python.org>
2013-01-11 08:43:40 -0800

    Move urlsplit() to curl.py.

--- curl.py
+++ curl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import urllib.parse
 
 import tulip
 from tulip import http_client
@@ -8,7 +9,14 @@ from tulip import http_client
 
 def main():
     url = sys.argv[1]
-    p = http_client.HttpClientProtocol(url)
+    scheme, netloc, path, query, fragment = urllib.parse.urlsplit(url)
+    if not path:
+        path = '/'
+    if query:
+        path = '?'.join([path, query])
+    print(netloc, path, scheme)
+    p = http_client.HttpClientProtocol(netloc, path=path,
+                                       ssl=(scheme=='https'))
     f = p.connect()
     p.event_loop.run_until_complete(tulip.Task(f))
 

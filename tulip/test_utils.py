cde1a544bc695d668539f782cb13bdb19dd20175
Guido van Rossum <guido@python.org>
2013-01-25 09:43:41 -0800

    Logging tweaks for tests.

---
+++ tulip/test_utils.py
@@ -0,0 +1,21 @@
+"""Utilities shared by tests."""
+
+import logging
+import unittest
+
+class LogTrackingTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self._logger = logging.getLogger()
+        self._log_level = self._logger.getEffectiveLevel()
+
+    def tearDown(self):
+        self._logger.setLevel(self._log_level)
+
+    def suppress_log_errors(self):
+        if self._log_level >= logging.WARNING:
+            self._logger.setLevel(logging.CRITICAL)
+
+    def suppress_log_warnings(self):
+        if self._log_level >= logging.WARNING:
+            self._logger.setLevel(logging.ERROR)

2c69c899d50edf4fdfd5168102511d076407b9d8
Guido van Rossum <guido@python.org>
2013-01-25 13:20:54 -0800

    Merge default -> iocp (mostly test improvements).

--- tulip/http_client_test.py
+++ tulip/http_client_test.py
@@ -5,6 +5,7 @@ import unittest
 from . import events
 from . import http_client
 from . import tasks
+from . import test_utils
 
 
 class StreamReaderTests(unittest.TestCase):
@@ -31,38 +32,105 @@ class StreamReaderTests(unittest.TestCase):
         self.assertEqual(self.DATA.count(b'\n'), stream.line_count)
         self.assertEqual(len(self.DATA), stream.byte_count)
 
+    @test_utils.sync
+    def test_read_zero(self):
+        """ Read zero bytes """
+        stream = http_client.StreamReader()
+        stream.feed_data(self.DATA)
+
+        data = yield from stream.read(0)
+        self.assertEqual(b'', data)
+        self.assertEqual(len(self.DATA), stream.byte_count)
+        self.assertEqual(self.DATA.count(b'\n'), stream.line_count)
+
+    @test_utils.sync
+    def test_read(self):
+        """ Read bytes """
+        stream = http_client.StreamReader()
+
+        res = stream.read(30)
+
+        def cb():
+            stream.feed_data(self.DATA)
+        self.event_loop.call_soon(cb)
+
+        data = yield from res
+        self.assertEqual(self.DATA, data)
+        self.assertFalse(stream.byte_count)
+        self.assertFalse(stream.line_count)
+
+    @test_utils.sync
+    def test_read_eof(self):
+        """ Read bytes, stop at eof """
+        stream = http_client.StreamReader()
+
+        read = tasks.Task(stream.read(1024))
+
+        def cb():
+            stream.feed_eof()
+        self.event_loop.call_soon(cb)
+
+        data = yield from read
+
+        self.assertEqual(b'', data)
+        self.assertFalse(stream.byte_count)
+        self.assertFalse(stream.line_count)
+
+    @test_utils.sync
+    def test_read_until_eof(self):
+        """ Read all bytes until eof """
+        stream = http_client.StreamReader()
+
+        read = tasks.Task(stream.read(-1))
+
+        def cb():
+            stream.feed_data(b'chunk1\n')
+            stream.feed_data(b'chunk2')
+            stream.feed_eof()
+        self.event_loop.call_soon(cb)
+
+        data = yield from read
+
+        self.assertEqual(b'chunk1\nchunk2', data)
+        self.assertFalse(stream.byte_count)
+        self.assertFalse(stream.line_count)
+
+    @test_utils.sync
+    def test_readline(self):
+        """ Read one line """
+        stream = http_client.StreamReader()
+        stream.feed_data(b'chunk1 ')
+
+        def cb():
+            stream.feed_data(b'chunk2 ')
+            stream.feed_data(b'chunk3 ')
+            stream.feed_data(b'\n chunk4')
+        self.event_loop.call_soon(cb)
+
+        line = yield from stream.readline()
+
+        self.assertEqual(b'chunk1 chunk2 chunk3 \n', line)
+        self.assertFalse(stream.line_count)
+        self.assertEqual(len(b'\n chunk4')-1, stream.byte_count)
+
+    @test_utils.sync
     def test_readline_line_byte_count(self):
         stream = http_client.StreamReader()
         stream.feed_data(self.DATA)
 
-        @tasks.coroutine
-        def readline():
-            line = yield from stream.readline()
-            return line
-
-        line = self.event_loop.run_until_complete(tasks.Task(readline()))
+        line = yield from stream.readline()
 
         self.assertEqual(b'line1\n', line)
         self.assertEqual(self.DATA.count(b'\n')-1, stream.line_count)
         self.assertEqual(len(self.DATA) - len(b'line1\n'), stream.byte_count)
 
+    @test_utils.sync
     def test_readline_read_byte_count(self):
         stream = http_client.StreamReader()
         stream.feed_data(self.DATA)
 
-        @tasks.coroutine
-        def readline():
-            line = yield from stream.readline()
-            return line
-
-        line = self.event_loop.run_until_complete(tasks.Task(readline()))
-
-        @tasks.coroutine
-        def read():
-            line = yield from stream.read(7)
-            return line
-
-        data = self.event_loop.run_until_complete(tasks.Task(read()))
+        line = yield from stream.readline()
+        data = yield from stream.read(7)
 
         self.assertEqual(b'line2\nl', data)
         self.assertEqual(
@@ -70,6 +138,57 @@ class StreamReaderTests(unittest.TestCase):
         self.assertEqual(
             len(self.DATA) - len(b'line1\n') - len(b'line2\nl'),
             stream.byte_count)
+
+    @test_utils.sync
+    def test_readexactly_zero_or_less(self):
+        """ Read exact number of bytes (zero or less) """
+        stream = http_client.StreamReader()
+        stream.feed_data(self.DATA)
+
+        data = yield from stream.readexactly(0)
+        self.assertEqual(b'', data)
+        self.assertEqual(len(self.DATA), stream.byte_count)
+        self.assertEqual(self.DATA.count(b'\n'), stream.line_count)
+
+        data = yield from stream.readexactly(-1)
+        self.assertEqual(b'', data)
+        self.assertEqual(len(self.DATA), stream.byte_count)
+        self.assertEqual(self.DATA.count(b'\n'), stream.line_count)
+
+    @test_utils.sync
+    def test_readexactly(self):
+        """ Read exact number of bytes """
+        stream = http_client.StreamReader()
+
+        def cb():
+            stream.feed_data(self.DATA)
+            stream.feed_data(self.DATA)
+            stream.feed_data(self.DATA)
+        self.event_loop.call_soon(cb)
+
+        n = 2*len(self.DATA)
+        data = yield from stream.readexactly(n)
+
+        self.assertEqual(self.DATA+self.DATA, data)
+        self.assertEqual(len(self.DATA), stream.byte_count)
+        self.assertEqual(self.DATA.count(b'\n'), stream.line_count)
+
+    @test_utils.sync
+    def test_readexactly_eof(self):
+        """ Read exact number of bytes (eof) """
+        stream = http_client.StreamReader()
+
+        def cb():
+            stream.feed_data(self.DATA)
+            stream.feed_eof()
+        self.event_loop.call_soon(cb)
+
+        n = 2*len(self.DATA)
+        data = yield from stream.readexactly(n)
+
+        self.assertEqual(self.DATA, data)
+        self.assertFalse(stream.byte_count)
+        self.assertFalse(stream.line_count)
 
 
 if __name__ == '__main__':
---
+++ tulip/test_utils.py
@@ -0,0 +1,34 @@
+"""Utilities shared by tests."""
+
+import functools
+import logging
+import unittest
+
+from . import events
+from . import tasks
+
+def sync(gen):
+    @functools.wraps(gen)
+    def wrapper(*args, **kw):
+        return events.get_event_loop().run_until_complete(
+            tasks.Task(gen(*args, **kw)))
+
+    return wrapper
+
+
+class LogTrackingTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self._logger = logging.getLogger()
+        self._log_level = self._logger.getEffectiveLevel()
+
+    def tearDown(self):
+        self._logger.setLevel(self._log_level)
+
+    def suppress_log_errors(self):
+        if self._log_level >= logging.WARNING:
+            self._logger.setLevel(logging.CRITICAL)
+
+    def suppress_log_warnings(self):
+        if self._log_level >= logging.WARNING:
+            self._logger.setLevel(logging.ERROR)

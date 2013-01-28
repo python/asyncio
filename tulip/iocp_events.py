108497b3167bf6d0ac019ffcb91f5d9ae8847122
Richard Oudkerk <shibturn@gmail.com>
2013-01-28 22:53:14 +0000

    Ignore ERROR_CONNECTION_ABORTED if transport has been closed.
    Plus changes suggested by Guido.

--- tulip/iocp_events.py
+++ tulip/iocp_events.py
@@ -15,24 +15,26 @@ import weakref
 
 from _winapi import CloseHandle
 
-from . import transports
 from . import events
+from . import futures
+from . import transports
+from . import unix_events
+from . import winsocketpair
+from . import _overlapped
 
-from .futures import Future
-from .unix_events import BaseEventLoop, _StopError
-from .winsocketpair import socketpair
-from ._overlapped import *
 
-
-_TRYAGAIN = frozenset()     # XXX
+NULL = 0
+INFINITE = 0xffffffff
+ERROR_CONNECTION_REFUSED = 1225
+ERROR_CONNECTION_ABORTED = 1236
 
 
 class IocpProactor:
 
     def __init__(self, concurrency=0xffffffff):
         self._results = []
-        self._iocp = CreateIoCompletionPort(
-            INVALID_HANDLE_VALUE, NULL, 0, concurrency)
+        self._iocp = _overlapped.CreateIoCompletionPort(
+            _overlapped.INVALID_HANDLE_VALUE, NULL, 0, concurrency)
         self._cache = {}
         self._registered = weakref.WeakSet()
 
@@ -47,54 +49,56 @@ class IocpProactor:
 
     def recv(self, conn, nbytes, flags=0):
         self._register_with_iocp(conn)
-        ov = Overlapped(NULL)
+        ov = _overlapped.Overlapped(NULL)
         ov.WSARecv(conn.fileno(), nbytes, flags)
         return self._register(ov, conn, ov.getresult)
 
     def send(self, conn, buf, flags=0):
         self._register_with_iocp(conn)
-        ov = Overlapped(NULL)
+        ov = _overlapped.Overlapped(NULL)
         ov.WSASend(conn.fileno(), buf, flags)
         return self._register(ov, conn, ov.getresult)
 
     def accept(self, listener):
         self._register_with_iocp(listener)
         conn = self._get_accept_socket()
-        ov = Overlapped(NULL)
+        ov = _overlapped.Overlapped(NULL)
         ov.AcceptEx(listener.fileno(), conn.fileno())
         def finish_accept():
             addr = ov.getresult()
             conn.setsockopt(socket.SOL_SOCKET,
-                            SO_UPDATE_ACCEPT_CONTEXT, listener.fileno())
+                            _overlapped.SO_UPDATE_ACCEPT_CONTEXT,
+                            listener.fileno())
             conn.settimeout(listener.gettimeout())
             return conn, conn.getpeername()
         return self._register(ov, listener, finish_accept)
 
     def connect(self, conn, address):
         self._register_with_iocp(conn)
-        BindLocal(conn.fileno(), len(address))
-        ov = Overlapped(NULL)
+        _overlapped.BindLocal(conn.fileno(), len(address))
+        ov = _overlapped.Overlapped(NULL)
         ov.ConnectEx(conn.fileno(), address)
         def finish_connect():
             try:
                 ov.getresult()
             except OSError as e:
-                if e.winerror == 1225:
+                if e.winerror == ERROR_CONNECTION_REFUSED:
                     raise ConnectionRefusedError(errno.ECONNREFUSED,
                                                  'connection refused')
                 raise
             conn.setsockopt(socket.SOL_SOCKET,
-                            SO_UPDATE_CONNECT_CONTEXT, 0)
+                            _overlapped.SO_UPDATE_CONNECT_CONTEXT,
+                            0)
             return conn
         return self._register(ov, conn, finish_connect)
 
     def _register_with_iocp(self, obj):
         if obj not in self._registered:
             self._registered.add(obj)
-            CreateIoCompletionPort(obj.fileno(), self._iocp, 0, 0)
+            _overlapped.CreateIoCompletionPort(obj.fileno(), self._iocp, 0, 0)
 
     def _register(self, ov, obj, callback):
-        f = Future()
+        f = futures.Future()
         self._cache[ov.address] = (f, ov, obj, callback)
         return f
 
@@ -113,7 +117,7 @@ class IocpProactor:
             if ms >= INFINITE:
                 raise ValueError("timeout too big")
         while True:
-            status = GetQueuedCompletionStatus(self._iocp, ms)
+            status = _overlapped.GetQueuedCompletionStatus(self._iocp, ms)
             if status is None:
                 return
             address = status[3]
@@ -172,6 +176,8 @@ class _IocpSocketTransport(transports.Transport):
                 self._event_loop.call_soon(self._protocol.data_received, data)
             self._read_fut = self._event_loop._proactor.recv(self._sock, 4096)
         except OSError as exc:
+            if exc.winerror == ERROR_CONNECTION_ABORTED and self._closing:
+                return
             self._fatal_error(exc)
         else:
             self._read_fut.add_done_callback(self._loop_reading)
@@ -230,7 +236,7 @@ class _IocpSocketTransport(transports.Transport):
             self._sock.close()
 
 
-class IocpEventLoop(BaseEventLoop):
+class IocpEventLoop(unix_events.BaseEventLoop):
 
     SocketTransport = _IocpSocketTransport
 
@@ -269,7 +275,7 @@ class IocpEventLoop(BaseEventLoop):
 
     def _make_self_pipe(self):
         # A self-socket, really. :-)
-        self._ssock, self._csock = socketpair()
+        self._ssock, self._csock = winsocketpair.socketpair()
         self._ssock.setblocking(False)
         self._csock.setblocking(False)
         def loop(f=None):
@@ -296,11 +302,8 @@ class IocpEventLoop(BaseEventLoop):
                     transport = self.SocketTransport(self, conn, protocol)
                 f = self._proactor.accept(sock)
             except OSError as exc:
-                if exc.errno in _TRYAGAIN:
-                    self.call_soon(loop)
-                else:
-                    sock.close()
-                    logging.exception('Accept failed')
+                sock.close()
+                logging.exception('Accept failed')
             else:
                 f.add_done_callback(loop)
         self.call_soon(loop)
--- tulip/unix_events.py
+++ tulip/unix_events.py
@@ -310,91 +310,6 @@ class BaseEventLoop(events.EventLoop):
         self._start_serving(protocol_factory, sock)
         return sock
 
-    def add_signal_handler(self, sig, callback, *args):
-        """Add a handler for a signal.  UNIX only.
-
-        Raise ValueError if the signal number is invalid or uncatchable.
-        Raise RuntimeError if there is a problem setting up the handler.
-        """
-        self._check_signal(sig)
-        try:
-            # set_wakeup_fd() raises ValueError if this is not the
-            # main thread.  By calling it early we ensure that an
-            # event loop running in another thread cannot add a signal
-            # handler.
-            signal.set_wakeup_fd(self._csock.fileno())
-        except ValueError as exc:
-            raise RuntimeError(str(exc))
-        handler = events.make_handler(None, callback, args)
-        self._signal_handlers[sig] = handler
-        try:
-            signal.signal(sig, self._handle_signal)
-        except OSError as exc:
-            del self._signal_handlers[sig]
-            if not self._signal_handlers:
-                try:
-                    signal.set_wakeup_fd(-1)
-                except ValueError as nexc:
-                    logging.info('set_wakeup_fd(-1) failed: %s', nexc)
-            if exc.errno == errno.EINVAL:
-                raise RuntimeError('sig {} cannot be caught'.format(sig))
-            else:
-                raise
-        return handler
-
-    def _handle_signal(self, sig, arg):
-        """Internal helper that is the actual signal handler."""
-        handler = self._signal_handlers.get(sig)
-        if handler is None:
-            return  # Assume it's some race condition.
-        if handler.cancelled:
-            self.remove_signal_handler(sig)  # Remove it properly.
-        else:
-            self.call_soon_threadsafe(handler.callback, *handler.args)
-
-    def remove_signal_handler(self, sig):
-        """Remove a handler for a signal.  UNIX only.
-
-        Return True if a signal handler was removed, False if not."""
-        self._check_signal(sig)
-        try:
-            del self._signal_handlers[sig]
-        except KeyError:
-            return False
-        if sig == signal.SIGINT:
-            handler = signal.default_int_handler
-        else:
-            handler = signal.SIG_DFL
-        try:
-            signal.signal(sig, handler)
-        except OSError as exc:
-            if exc.errno == errno.EINVAL:
-                raise RuntimeError('sig {} cannot be caught'.format(sig))
-            else:
-                raise
-        if not self._signal_handlers:
-            try:
-                signal.set_wakeup_fd(-1)
-            except ValueError as exc:
-                logging.info('set_wakeup_fd(-1) failed: %s', exc)
-        return True
-
-    def _check_signal(self, sig):
-        """Internal helper to validate a signal.
-
-        Raise ValueError if the signal number is invalid or uncatchable.
-        Raise RuntimeError if there is a problem setting up the handler.
-        """
-        if not isinstance(sig, int):
-            raise TypeError('sig must be an int, not {!r}'.format(sig))
-        if signal is None:
-            raise RuntimeError('Signals are not supported')
-        if not (1 <= sig < signal.NSIG):
-            raise ValueError('sig {} out of range(1, {})'.format(sig,
-                                                                 signal.NSIG))
-        if sys.platform == 'win32':
-            raise RuntimeError('Signals are not really supported on Windows')
-
     def _add_callback(self, handler):
         """Add a Handler to ready or scheduled."""
         if handler.cancelled:
@@ -789,6 +704,91 @@ class UnixEventLoop(BaseEventLoop):
                 else:
                     self._add_callback(connector)
 
+    def add_signal_handler(self, sig, callback, *args):
+        """Add a handler for a signal.  UNIX only.
+
+        Raise ValueError if the signal number is invalid or uncatchable.
+        Raise RuntimeError if there is a problem setting up the handler.
+        """
+        self._check_signal(sig)
+        try:
+            # set_wakeup_fd() raises ValueError if this is not the
+            # main thread.  By calling it early we ensure that an
+            # event loop running in another thread cannot add a signal
+            # handler.
+            signal.set_wakeup_fd(self._csock.fileno())
+        except ValueError as exc:
+            raise RuntimeError(str(exc))
+        handler = events.make_handler(None, callback, args)
+        self._signal_handlers[sig] = handler
+        try:
+            signal.signal(sig, self._handle_signal)
+        except OSError as exc:
+            del self._signal_handlers[sig]
+            if not self._signal_handlers:
+                try:
+                    signal.set_wakeup_fd(-1)
+                except ValueError as nexc:
+                    logging.info('set_wakeup_fd(-1) failed: %s', nexc)
+            if exc.errno == errno.EINVAL:
+                raise RuntimeError('sig {} cannot be caught'.format(sig))
+            else:
+                raise
+        return handler
+
+    def _handle_signal(self, sig, arg):
+        """Internal helper that is the actual signal handler."""
+        handler = self._signal_handlers.get(sig)
+        if handler is None:
+            return  # Assume it's some race condition.
+        if handler.cancelled:
+            self.remove_signal_handler(sig)  # Remove it properly.
+        else:
+            self.call_soon_threadsafe(handler.callback, *handler.args)
+
+    def remove_signal_handler(self, sig):
+        """Remove a handler for a signal.  UNIX only.
+
+        Return True if a signal handler was removed, False if not."""
+        self._check_signal(sig)
+        try:
+            del self._signal_handlers[sig]
+        except KeyError:
+            return False
+        if sig == signal.SIGINT:
+            handler = signal.default_int_handler
+        else:
+            handler = signal.SIG_DFL
+        try:
+            signal.signal(sig, handler)
+        except OSError as exc:
+            if exc.errno == errno.EINVAL:
+                raise RuntimeError('sig {} cannot be caught'.format(sig))
+            else:
+                raise
+        if not self._signal_handlers:
+            try:
+                signal.set_wakeup_fd(-1)
+            except ValueError as exc:
+                logging.info('set_wakeup_fd(-1) failed: %s', exc)
+        return True
+
+    def _check_signal(self, sig):
+        """Internal helper to validate a signal.
+
+        Raise ValueError if the signal number is invalid or uncatchable.
+        Raise RuntimeError if there is a problem setting up the handler.
+        """
+        if not isinstance(sig, int):
+            raise TypeError('sig must be an int, not {!r}'.format(sig))
+        if signal is None:
+            raise RuntimeError('Signals are not supported')
+        if not (1 <= sig < signal.NSIG):
+            raise ValueError('sig {} out of range(1, {})'.format(sig,
+                                                                 signal.NSIG))
+        if sys.platform == 'win32':
+            raise RuntimeError('Signals are not really supported on Windows')
+
 
 class _UnixSocketTransport(transports.Transport):
 
@@ -815,7 +815,6 @@ class _UnixSocketTransport(transports.Transport):
             else:
                 self._event_loop.remove_reader(self._sock.fileno())
                 self._event_loop.call_soon(self._protocol.eof_received)
-
 
     def write(self, data):
         assert isinstance(data, bytes)

3b22ab324b85f19b7110b6bac88511ee6c582db0
Nikolay Kim <fafhrd91@gmail.com>
2013-02-11 10:12:02 -0800

    Synchronization primitives

--- tulip/__init__.py
+++ tulip/__init__.py
@@ -3,12 +3,14 @@
 # This relies on each of the submodules having an __all__ variable.
 from .futures import *
 from .events import *
+from .locks import *
 from .transports import *
 from .protocols import *
 from .tasks import *
 
 __all__ = (futures.__all__ +
            events.__all__ +
+           locks.__all__ +
            transports.__all__ +
            protocols.__all__ +
            tasks.__all__)

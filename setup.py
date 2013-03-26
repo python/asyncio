381dd7906698bb52cbedf3e70ff33c9c4b3dd9e1
Guido van Rossum <guido@python.org>
2013-03-26 09:09:15 -0700

    Improved setup.py from issue 23.

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import os
 from distutils.core import setup, Extension
 
-ext = Extension('_overlapped', ['overlapped.c'], libraries=['ws2_32'])
-setup(name='_overlapped', ext_modules=[ext])
+extensions = []
+if os.name == 'nt':
+    ext = Extension('_overlapped', ['overlapped.c'], libraries=['ws2_32'])
+    extensions.append(ext)
+
+setup(name='tulip',
+      description="reference implementation of PEP 3156",
+      url='http://www.python.org/dev/peps/pep-3156/',
+      packages=['tulip'],
+      ext_modules=extensions
+     )

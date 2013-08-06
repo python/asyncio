f46798a7ba72a1508ade73027966c21648ae172e
Guido van Rossum <guido@python.org>
2013-08-06 16:51:34 -0700

    Break long lines.

--- tests/http_client_functional_test.py
+++ tests/http_client_functional_test.py
@@ -29,7 +29,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
         with test_utils.run_test_server(self.loop, router=Functional) as httpd:
             for meth in ('get', 'post', 'put', 'delete', 'head'):
                 r = self.loop.run_until_complete(
-                    client.request(meth, httpd.url('method', meth), loop=self.loop))
+                    client.request(meth, httpd.url('method', meth),
+                                   loop=self.loop))
                 content1 = self.loop.run_until_complete(r.read())
                 content2 = self.loop.run_until_complete(r.read())
                 content = content1.decode()
@@ -42,7 +43,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
     def test_HTTP_302_REDIRECT_GET(self):
         with test_utils.run_test_server(self.loop, router=Functional) as httpd:
             r = self.loop.run_until_complete(
-                client.request('get', httpd.url('redirect', 2), loop=self.loop))
+                client.request('get', httpd.url('redirect', 2),
+                               loop=self.loop))
 
             self.assertEqual(r.status, 200)
             self.assertEqual(2, httpd['redirects'])
@@ -52,7 +54,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
             self.assertRaises(
                 ValueError,
                 self.loop.run_until_complete,
-                client.request('get', httpd.url('redirect_err'), loop=self.loop))
+                client.request('get', httpd.url('redirect_err'),
+                               loop=self.loop))
 
     def test_HTTP_302_REDIRECT_POST(self):
         with test_utils.run_test_server(self.loop, router=Functional) as httpd:
@@ -102,7 +105,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
         with test_utils.run_test_server(self.loop, router=Functional) as httpd:
             url = httpd.url('method', 'post')
             r = self.loop.run_until_complete(
-                client.request('post', url, data={'some': 'data'}, loop=self.loop))
+                client.request('post', url, data={'some': 'data'},
+                               loop=self.loop))
             self.assertEqual(r.status, 200)
 
             content = self.loop.run_until_complete(r.read(True))
@@ -114,7 +118,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
             url = httpd.url('method', 'post')
             r = self.loop.run_until_complete(
                 client.request('post', url,
-                               data={'some': 'data'}, compress=True, loop=self.loop))
+                               data={'some': 'data'}, compress=True,
+                               loop=self.loop))
             self.assertEqual(r.status, 200)
 
             content = self.loop.run_until_complete(r.read(True))
@@ -130,7 +135,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
                 r = self.loop.run_until_complete(
                     client.request(
                         'post', url, files={'some': f}, chunked=1024,
-                        headers={'Transfer-Encoding': 'chunked'}, loop=self.loop))
+                        headers={'Transfer-Encoding': 'chunked'},
+                        loop=self.loop))
                 content = self.loop.run_until_complete(r.read(True))
 
                 f.seek(0)
@@ -152,7 +158,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
             with open(__file__) as f:
                 r = self.loop.run_until_complete(
                     client.request('post', url, files={'some': f},
-                                   chunked=1024, compress='deflate', loop=self.loop))
+                                   chunked=1024, compress='deflate',
+                                   loop=self.loop))
 
                 content = self.loop.run_until_complete(r.read(True))
 
@@ -175,7 +182,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
 
             with open(__file__) as f:
                 r = self.loop.run_until_complete(
-                    client.request('post', url, files=[('some', f.read())], loop=self.loop))
+                    client.request('post', url, files=[('some', f.read())],
+                                   loop=self.loop))
 
                 content = self.loop.run_until_complete(r.read(True))
 
@@ -195,7 +203,8 @@ class HttpClientFunctionalTests(unittest.TestCase):
 
             with open(__file__) as f:
                 r = self.loop.run_until_complete(
-                    client.request('post', url, files=[('some', f)], loop=self.loop))
+                    client.request('post', url, files=[('some', f)],
+                                   loop=self.loop))
 
                 content = self.loop.run_until_complete(r.read(True))
 

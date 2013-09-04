b9a148bb30dc39644b40db11400404ad70a60300
Andrew Svetlov <andrew.svetlov@gmail.com>
2013-09-04 07:51:09 +0300

    Fix queues cancellation.

---
+++ examples/tcp_echo.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+"""TCP echo server example."""
+import argparse
+import tulip
+try:
+    import signal
+except ImportError:
+    signal = None
+
+
+class EchoServer(tulip.Protocol):
+
+    TIMEOUT = 5.0
+
+    def timeout(self):
+        print('connection timeout, closing.')
+        self.transport.close()
+
+    def connection_made(self, transport):
+        print('connection made')
+        self.transport = transport
+
+        # start 5 seconds timeout timer
+        self.h_timeout = tulip.get_event_loop().call_later(
+            self.TIMEOUT, self.timeout)
+
+    def data_received(self, data):
+        print('data received: ', data.decode())
+        self.transport.write(b'Re: ' + data)
+
+        # restart timeout timer
+        self.h_timeout.cancel()
+        self.h_timeout = tulip.get_event_loop().call_later(
+            self.TIMEOUT, self.timeout)
+
+    def eof_received(self):
+        pass
+
+    def connection_lost(self, exc):
+        print('connection lost:', exc)
+        self.h_timeout.cancel()
+
+
+class EchoClient(tulip.Protocol):
+
+    message = 'This is the message. It will be echoed.'
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.transport.write(self.message.encode())
+        print('data sent:', self.message)
+
+    def data_received(self, data):
+        print('data received:', data)
+
+        # disconnect after 10 seconds
+        tulip.get_event_loop().call_later(10.0, self.transport.close)
+
+    def eof_received(self):
+        pass
+
+    def connection_lost(self, exc):
+        print('connection lost:', exc)
+        tulip.get_event_loop().stop()
+
+
+def start_client(loop, host, port):
+    t = tulip.Task(loop.create_connection(EchoClient, host, port))
+    loop.run_until_complete(t)
+
+
+def start_server(loop, host, port):
+    f = loop.start_serving(EchoServer, host, port)
+    x = loop.run_until_complete(f)[0]
+    print('serving on', x.getsockname())
+
+
+ARGS = argparse.ArgumentParser(description="TCP Echo example.")
+ARGS.add_argument(
+    '--server', action="store_true", dest='server',
+    default=False, help='Run tcp server')
+ARGS.add_argument(
+    '--client', action="store_true", dest='client',
+    default=False, help='Run tcp client')
+ARGS.add_argument(
+    '--host', action="store", dest='host',
+    default='127.0.0.1', help='Host name')
+ARGS.add_argument(
+    '--port', action="store", dest='port',
+    default=9999, type=int, help='Port number')
+
+
+if __name__ == '__main__':
+    args = ARGS.parse_args()
+
+    if ':' in args.host:
+        args.host, port = args.host.split(':', 1)
+        args.port = int(port)
+
+    if (not (args.server or args.client)) or (args.server and args.client):
+        print('Please specify --server or --client\n')
+        ARGS.print_help()
+    else:
+        loop = tulip.get_event_loop()
+        if signal is not None:
+            loop.add_signal_handler(signal.SIGINT, loop.stop)
+
+        if args.server:
+            start_server(loop, args.host, args.port)
+        else:
+            start_client(loop, args.host, args.port)
+
+        loop.run_forever()
---
+++ tulip/proactor_events.py
@@ -0,0 +1,288 @@
+"""Event loop using a proactor and related classes.
+
+A proactor is a "notify-on-completion" multiplexer.  Currently a
+proactor is only implemented on Windows with IOCP.
+"""
+
+import socket
+
+from . import base_events
+from . import constants
+from . import futures
+from . import transports
+from .log import tulip_log
+
+
+class _ProactorBasePipeTransport(transports.BaseTransport):
+    """Base class for pipe and socket transports."""
+
+    def __init__(self, loop, sock, protocol, waiter=None, extra=None):
+        super().__init__(extra)
+        self._set_extra(sock)
+        self._loop = loop
+        self._sock = sock
+        self._protocol = protocol
+        self._buffer = []
+        self._read_fut = None
+        self._write_fut = None
+        self._writing_disabled = False
+        self._conn_lost = 0
+        self._closing = False  # Set when close() called.
+        self._loop.call_soon(self._protocol.connection_made, self)
+        if waiter is not None:
+            self._loop.call_soon(waiter.set_result, None)
+
+    def _set_extra(self, sock):
+        self._extra['pipe'] = sock
+
+    def close(self):
+        if self._closing:
+            return
+        self._closing = True
+        self._conn_lost += 1
+        if not self._buffer and self._write_fut is None:
+            self._loop.call_soon(self._call_connection_lost, None)
+        if self._read_fut is not None:
+            self._read_fut.cancel()
+
+    def _fatal_error(self, exc):
+        tulip_log.exception('Fatal error for %s', self)
+        self._force_close(exc)
+
+    def _force_close(self, exc):
+        if self._closing:
+            return
+        self._closing = True
+        self._conn_lost += 1
+        if self._write_fut:
+            self._write_fut.cancel()
+        if self._read_fut:
+            self._read_fut.cancel()
+        self._write_fut = self._read_fut = None
+        self._buffer = []
+        self._loop.call_soon(self._call_connection_lost, exc)
+
+    def _call_connection_lost(self, exc):
+        try:
+            self._protocol.connection_lost(exc)
+        finally:
+            # XXX If there is a pending overlapped read on the other
+            # end then it may fail with ERROR_NETNAME_DELETED if we
+            # just close our end.  First calling shutdown() seems to
+            # cure it, but maybe using DisconnectEx() would be better.
+            if hasattr(self._sock, 'shutdown'):
+                self._sock.shutdown(socket.SHUT_RDWR)
+            self._sock.close()
+
+
+class _ProactorReadPipeTransport(_ProactorBasePipeTransport,
+                                 transports.ReadTransport):
+    """Transport for read pipes."""
+
+    def __init__(self, loop, sock, protocol, waiter=None, extra=None):
+        super().__init__(loop, sock, protocol, waiter, extra)
+        self._loop.call_soon(self._loop_reading)
+
+    def _loop_reading(self, fut=None):
+        data = None
+
+        try:
+            if fut is not None:
+                assert fut is self._read_fut
+                self._read_fut = None
+                data = fut.result()  # deliver data later in "finally" clause
+
+            if self._closing:
+                # since close() has been called we ignore any read data
+                data = None
+                return
+
+            if data == b'':
+                # we got end-of-file so no need to reschedule a new read
+                return
+
+            # reschedule a new read
+            self._read_fut = self._loop._proactor.recv(self._sock, 4096)
+        except ConnectionAbortedError as exc:
+            if not self._closing:
+                self._fatal_error(exc)
+        except ConnectionResetError as exc:
+            self._force_close(exc)
+        except OSError as exc:
+            self._fatal_error(exc)
+        except futures.CancelledError:
+            if not self._closing:
+                raise
+        else:
+            self._read_fut.add_done_callback(self._loop_reading)
+        finally:
+            if data:
+                self._protocol.data_received(data)
+            elif data is not None:
+                try:
+                    self._protocol.eof_received()
+                finally:
+                    self.close()
+
+
+class _ProactorWritePipeTransport(_ProactorBasePipeTransport,
+                                  transports.WriteTransport):
+    """Transport for write pipes."""
+
+    def write(self, data):
+        assert isinstance(data, bytes), repr(data)
+        if not data:
+            return
+
+        if self._conn_lost:
+            if self._conn_lost >= constants.LOG_THRESHOLD_FOR_CONNLOST_WRITES:
+                tulip_log.warning('socket.send() raised exception.')
+            self._conn_lost += 1
+            return
+        self._buffer.append(data)
+        if self._write_fut is None and not self._writing_disabled:
+            self._loop_writing()
+
+    def _loop_writing(self, f=None):
+        try:
+            assert f is self._write_fut
+            self._write_fut = None
+            if f:
+                f.result()
+            data = b''.join(self._buffer)
+            self._buffer = []
+            if not data:
+                if self._closing:
+                    self._loop.call_soon(self._call_connection_lost, None)
+                return
+            if not self._writing_disabled:
+                self._write_fut = self._loop._proactor.send(self._sock, data)
+                self._write_fut.add_done_callback(self._loop_writing)
+        except OSError as exc:
+            self._fatal_error(exc)
+
+    # TODO: write_eof(), can_write_eof().
+
+    def abort(self):
+        self._force_close(None)
+
+    def pause_writing(self):
+        self._writing_disabled = True
+
+    def resume_writing(self):
+        self._writing_disabled = False
+        if self._buffer and self._write_fut is None:
+            self._loop_writing()
+
+    def discard_output(self):
+        if self._buffer:
+            self._buffer = []
+
+
+class _ProactorSocketTransport(_ProactorReadPipeTransport,
+                               _ProactorWritePipeTransport,
+                               transports.Transport):
+    """Transport for connected sockets."""
+
+    def _set_extra(self, sock):
+        self._extra['socket'] = sock
+
+
+class BaseProactorEventLoop(base_events.BaseEventLoop):
+
+    def __init__(self, proactor):
+        super().__init__()
+        tulip_log.debug('Using proactor: %s', proactor.__class__.__name__)
+        self._proactor = proactor
+        self._selector = proactor   # convenient alias
+        proactor.set_loop(self)
+        self._make_self_pipe()
+
+    def _make_socket_transport(self, sock, protocol, waiter=None, extra=None):
+        return _ProactorSocketTransport(self, sock, protocol, waiter, extra)
+
+    def _make_read_pipe_transport(self, sock, protocol, waiter=None,
+                                  extra=None):
+        return _ProactorReadPipeTransport(self, sock, protocol, waiter, extra)
+
+    def _make_write_pipe_transport(self, sock, protocol, waiter=None,
+                                   extra=None):
+        return _ProactorWritePipeTransport(self, sock, protocol, waiter, extra)
+
+    def close(self):
+        if self._proactor is not None:
+            self._close_self_pipe()
+            self._proactor.close()
+            self._proactor = None
+            self._selector = None
+
+    def sock_recv(self, sock, n):
+        return self._proactor.recv(sock, n)
+
+    def sock_sendall(self, sock, data):
+        return self._proactor.send(sock, data)
+
+    def sock_connect(self, sock, address):
+        return self._proactor.connect(sock, address)
+
+    def sock_accept(self, sock):
+        return self._proactor.accept(sock)
+
+    def _socketpair(self):
+        raise NotImplementedError
+
+    def _close_self_pipe(self):
+        self._ssock.close()
+        self._ssock = None
+        self._csock.close()
+        self._csock = None
+        self._internal_fds -= 1
+
+    def _make_self_pipe(self):
+        # A self-socket, really. :-)
+        self._ssock, self._csock = self._socketpair()
+        self._ssock.setblocking(False)
+        self._csock.setblocking(False)
+        self._internal_fds += 1
+        self.call_soon(self._loop_self_reading)
+
+    def _loop_self_reading(self, f=None):
+        try:
+            if f is not None:
+                f.result()  # may raise
+            f = self._proactor.recv(self._ssock, 4096)
+        except:
+            self.close()
+            raise
+        else:
+            f.add_done_callback(self._loop_self_reading)
+
+    def _write_to_self(self):
+        self._csock.send(b'x')
+
+    def _start_serving(self, protocol_factory, sock, ssl=None):
+        assert not ssl, 'IocpEventLoop imcompatible with SSL.'
+
+        def loop(f=None):
+            try:
+                if f:
+                    conn, addr = f.result()
+                    protocol = protocol_factory()
+                    self._make_socket_transport(
+                        conn, protocol, extra={'addr': addr})
+                f = self._proactor.accept(sock)
+            except OSError:
+                sock.close()
+                tulip_log.exception('Accept failed')
+            except futures.CancelledError:
+                sock.close()
+            else:
+                f.add_done_callback(loop)
+        self.call_soon(loop)
+
+    def _process_events(self, event_list):
+        pass    # XXX hard work currently done in poll
+
+    def stop_serving(self, sock):
+        self._proactor.stop_serving(sock)
+        sock.close()

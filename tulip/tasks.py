81465772d2e2930493a69b87b7a390647165fa2a
Antoine Pitrou <solipsis@pitrou.net>
2013-09-06 21:00:49 +0200

    Add tasks.gather(), similar to Twisted's gatherResults().

---
+++ tulip/tasks.py
@@ -0,0 +1,377 @@
+"""Support for tasks, coroutines and the scheduler."""
+
+__all__ = ['coroutine', 'Task',
+           'FIRST_COMPLETED', 'FIRST_EXCEPTION', 'ALL_COMPLETED',
+           'wait', 'wait_for', 'as_completed', 'sleep', 'async',
+           'gather',
+           ]
+
+import collections
+import concurrent.futures
+import functools
+import inspect
+
+from . import events
+from . import futures
+
+
+def coroutine(func):
+    """Decorator to mark coroutines.
+
+    Decorator wraps non generator functions and returns generator wrapper.
+    If non generator function returns generator of Future it yield-from it.
+
+    TODO: This is a feel-good API only. It is not enforced.
+    """
+    if inspect.isgeneratorfunction(func):
+        coro = func
+    else:
+        @functools.wraps(func)
+        def coro(*args, **kw):
+            res = func(*args, **kw)
+            if isinstance(res, futures.Future) or inspect.isgenerator(res):
+                res = yield from res
+            return res
+
+    coro._is_coroutine = True  # Not sure who can use this.
+    return coro
+
+
+# TODO: Do we need this?
+def iscoroutinefunction(func):
+    """Return True if func is a decorated coroutine function."""
+    return (inspect.isgeneratorfunction(func) and
+            getattr(func, '_is_coroutine', False))
+
+
+# TODO: Do we need this?
+def iscoroutine(obj):
+    """Return True if obj is a coroutine object."""
+    return inspect.isgenerator(obj)  # TODO: And what?
+
+
+class Task(futures.Future):
+    """A coroutine wrapped in a Future."""
+
+    def __init__(self, coro, *, loop=None):
+        assert inspect.isgenerator(coro)  # Must be a coroutine *object*.
+        super().__init__(loop=loop)
+        self._coro = coro
+        self._fut_waiter = None
+        self._must_cancel = False
+        self._loop.call_soon(self._step)
+
+    def __repr__(self):
+        res = super().__repr__()
+        if (self._must_cancel and
+            self._state == futures._PENDING and
+            '<PENDING' in res):
+            res = res.replace('<PENDING', '<CANCELLING', 1)
+        i = res.find('<')
+        if i < 0:
+            i = len(res)
+        res = res[:i] + '(<{}>)'.format(self._coro.__name__) + res[i:]
+        return res
+
+    def cancel(self):
+        if self.done():
+            return False
+        if self._fut_waiter is not None:
+            if self._fut_waiter.cancel():
+                return True
+        # It must be the case that self._step is already scheduled.
+        self._must_cancel = True
+        return True
+
+    def _step(self, value=None, exc=None):
+        assert not self.done(), \
+            '_step(): already done: {!r}, {!r}, {!r}'.format(self, value, exc)
+        if self._must_cancel:
+            assert self._fut_waiter is None
+            exc = futures.CancelledError()
+            value = None
+        coro = self._coro
+        self._fut_waiter = None
+        # Call either coro.throw(exc) or coro.send(value).
+        try:
+            if exc is not None:
+                result = coro.throw(exc)
+            elif value is not None:
+                result = coro.send(value)
+            else:
+                result = next(coro)
+        except StopIteration as exc:
+            self.set_result(exc.value)
+        except futures.CancelledError as exc:
+            super().cancel()  # I.e., Future.cancel(self).
+        except Exception as exc:
+            self.set_exception(exc)
+        except BaseException as exc:
+            self.set_exception(exc)
+            raise
+        else:
+            if isinstance(result, futures.Future):
+                # Yielded Future must come from Future.__iter__().
+                if result._blocking:
+                    result._blocking = False
+                    result.add_done_callback(self._wakeup)
+                    self._fut_waiter = result
+                else:
+                    self._loop.call_soon(
+                        self._step, None,
+                        RuntimeError(
+                            'yield was used instead of yield from '
+                            'in task {!r} with {!r}'.format(self, result)))
+            elif result is None:
+                # Bare yield relinquishes control for one event loop iteration.
+                self._loop.call_soon(self._step)
+            elif inspect.isgenerator(result):
+                # Yielding a generator is just wrong.
+                self._loop.call_soon(
+                    self._step, None,
+                    RuntimeError(
+                        'yield was used instead of yield from for '
+                        'generator in task {!r} with {}'.format(
+                            self, result)))
+            else:
+                # Yielding something else is an error.
+                self._loop.call_soon(
+                    self._step, None,
+                    RuntimeError(
+                        'Task got bad yield: {!r}'.format(result)))
+        self = None
+
+    def _wakeup(self, future):
+        try:
+            value = future.result()
+        except Exception as exc:
+            self._step(None, exc)
+        else:
+            self._step(value, None)
+        self = None  # Needed to break cycles when an exception occurs.
+
+
+# wait() and as_completed() similar to those in PEP 3148.
+
+FIRST_COMPLETED = concurrent.futures.FIRST_COMPLETED
+FIRST_EXCEPTION = concurrent.futures.FIRST_EXCEPTION
+ALL_COMPLETED = concurrent.futures.ALL_COMPLETED
+
+
+@coroutine
+def wait(fs, *, loop=None, timeout=None, return_when=ALL_COMPLETED):
+    """Wait for the Futures and coroutines given by fs to complete.
+
+    Coroutines will be wrapped in Tasks.
+
+    Returns two sets of Future: (done, pending).
+
+    Usage:
+
+        done, pending = yield from tulip.wait(fs)
+
+    Note: This does not raise TimeoutError! Futures that aren't done
+    when the timeout occurs are returned in the second set.
+    """
+    if not fs:
+        raise ValueError('Set of coroutines/Futures is empty.')
+
+    if loop is None:
+        loop = events.get_event_loop()
+
+    fs = set(async(f, loop=loop) for f in fs)
+
+    if return_when not in (FIRST_COMPLETED, FIRST_EXCEPTION, ALL_COMPLETED):
+        raise ValueError('Invalid return_when value: {}'.format(return_when))
+    return (yield from _wait(fs, timeout, return_when, loop))
+
+
+@coroutine
+def wait_for(fut, timeout, *, loop=None):
+    """Wait for the single Future or coroutine to complete, with timeout.
+
+    Coroutine will be wrapped in Task.
+
+    Returns result of the Future or coroutine.  Raises TimeoutError when
+    timeout occurs.
+
+    Usage:
+
+        result = yield from tulip.wait_for(fut, 10.0)
+
+    """
+    if loop is None:
+        loop = events.get_event_loop()
+
+    fut = async(fut, loop=loop)
+
+    done, pending = yield from _wait([fut], timeout, FIRST_COMPLETED, loop)
+    if done:
+        return done.pop().result()
+
+    raise futures.TimeoutError()
+
+
+@coroutine
+def _wait(fs, timeout, return_when, loop):
+    """Internal helper for wait(return_when=FIRST_COMPLETED).
+
+    The fs argument must be a set of Futures.
+    The timeout argument is like for wait().
+    """
+    assert fs, 'Set of Futures is empty.'
+    waiter = futures.Future(loop=loop)
+    timeout_handle = None
+    if timeout is not None:
+        timeout_handle = loop.call_later(timeout, waiter.cancel)
+    counter = len(fs)
+
+    def _on_completion(f):
+        nonlocal counter
+        counter -= 1
+        if (counter <= 0 or
+            return_when == FIRST_COMPLETED or
+            return_when == FIRST_EXCEPTION and (not f.cancelled() and
+                                                f.exception() is not None)):
+            if timeout_handle is not None:
+                timeout_handle.cancel()
+            waiter.cancel()
+
+    for f in fs:
+        f.add_done_callback(_on_completion)
+    try:
+        yield from waiter
+    except futures.CancelledError:
+        pass
+    done, pending = set(), set()
+    for f in fs:
+        f.remove_done_callback(_on_completion)
+        if f.done():
+            done.add(f)
+        else:
+            pending.add(f)
+    return done, pending
+
+
+# This is *not* a @coroutine!  It is just an iterator (yielding Futures).
+def as_completed(fs, *, loop=None, timeout=None):
+    """Return an iterator whose values, when waited for, are Futures.
+
+    This differs from PEP 3148; the proper way to use this is:
+
+        for f in as_completed(fs):
+            result = yield from f  # The 'yield from' may raise.
+            # Use result.
+
+    Raises TimeoutError if the timeout occurs before all Futures are
+    done.
+
+    Note: The futures 'f' are not necessarily members of fs.
+    """
+    loop = loop if loop is not None else events.get_event_loop()
+    deadline = None if timeout is None else loop.time() + timeout
+    todo = set(async(f, loop=loop) for f in fs)
+    completed = collections.deque()
+
+    @coroutine
+    def _wait_for_one():
+        while not completed:
+            timeout = None
+            if deadline is not None:
+                timeout = deadline - loop.time()
+                if timeout < 0:
+                    raise futures.TimeoutError()
+            done, pending = yield from _wait(
+                todo, timeout, FIRST_COMPLETED, loop)
+            # Multiple callers might be waiting for the same events
+            # and getting the same outcome.  Dedupe by updating todo.
+            for f in done:
+                if f in todo:
+                    todo.remove(f)
+                    completed.append(f)
+        f = completed.popleft()
+        return f.result()  # May raise.
+
+    for _ in range(len(todo)):
+        yield _wait_for_one()
+
+
+@coroutine
+def sleep(delay, result=None, *, loop=None):
+    """Coroutine that completes after a given time (in seconds)."""
+    future = futures.Future(loop=loop)
+    h = future._loop.call_later(delay, future.set_result, result)
+    try:
+        return (yield from future)
+    finally:
+        h.cancel()
+
+
+def async(coro_or_future, *, loop=None):
+    """Wrap a coroutine in a future.
+
+    If the argument is a Future, it is returned directly.
+    """
+    if isinstance(coro_or_future, futures.Future):
+        if loop is not None and loop is not coro_or_future._loop:
+            raise ValueError('loop argument must agree with Future')
+        return coro_or_future
+    elif iscoroutine(coro_or_future):
+        return Task(coro_or_future, loop=loop)
+    else:
+        raise TypeError('A Future or coroutine is required')
+
+
+def gather(*coros_or_futures, loop=None, return_exceptions=False):
+    """Return a future aggregating results from the given coroutines
+    or futures.
+
+    All futures must share the same event loop.  If all the tasks
+    are done successfully, the returned future's result is the list of
+    results (in the order of the original sequence, not necessarily the
+    order of results arrival).  If one of the tasks is cancelled, the
+    returned future is immediately cancelled too.  If *result_exception*
+    is True, exceptions in the tasks are treated the same as successful
+    results, and gathered in the result list; otherwise, the first raised
+    exception will be immediately propagated to the returned future.
+    """
+    children = [async(fut, loop=loop) for fut in coros_or_futures]
+    n = len(children)
+    if n == 0:
+        outer = futures.Future(loop=loop)
+        outer.set_result([])
+        return outer
+    if loop is None:
+        loop = children[0]._loop
+    for fut in children:
+        if fut._loop is not loop:
+            raise ValueError("futures are tied to different event loops")
+    outer = futures.Future(loop=loop)
+    nfinished = 0
+    results = [None] * n
+
+    def _done_callback(i, fut):
+        nonlocal nfinished
+        if outer._state != futures._PENDING:
+            if fut._exception is not None:
+                # Be sure to mark the result retrieved
+                fut.exception()
+            return
+        if fut._state == futures._CANCELLED:
+            outer.cancel()
+            return
+        elif fut._exception is not None:
+            if not return_exceptions:
+                outer.set_exception(fut.exception())
+                return
+            res = fut.exception()
+        else:
+            res = fut._result
+        results[i] = res
+        nfinished += 1
+        if nfinished == n:
+            outer.set_result(results)
+
+    for i, fut in enumerate(children):
+        fut.add_done_callback(functools.partial(_done_callback, i))
+    return outer
---
+++ tulip/test_utils.py
@@ -0,0 +1,443 @@
+"""Utilities shared by tests."""
+
+import cgi
+import collections
+import contextlib
+import gc
+import email.parser
+import http.server
+import json
+import logging
+import io
+import unittest.mock
+import os
+import re
+import socket
+import sys
+import threading
+import traceback
+import unittest
+import unittest.mock
+import urllib.parse
+try:
+    import ssl
+except ImportError:  # pragma: no cover
+    ssl = None
+
+import tulip
+import tulip.http
+from tulip.http import client
+from tulip import base_events
+from tulip import events
+
+from tulip import base_events
+from tulip import selectors
+
+
+if sys.platform == 'win32':  # pragma: no cover
+    from .windows_utils import socketpair
+else:
+    from socket import socketpair  # pragma: no cover
+
+
+def run_briefly(loop):
+    @tulip.coroutine
+    def once():
+        pass
+    t = tulip.Task(once(), loop=loop)
+    loop.run_until_complete(t)
+
+
+def run_once(loop):
+    """loop.stop() schedules _raise_stop_error()
+    and run_forever() runs until _raise_stop_error() callback.
+    this wont work if test waits for some IO events, because
+    _raise_stop_error() runs before any of io events callbacks.
+    """
+    loop.stop()
+    loop.run_forever()
+
+
+@contextlib.contextmanager
+def run_test_server(loop, *, host='127.0.0.1', port=0,
+                    use_ssl=False, router=None):
+    properties = {}
+    transports = []
+
+    class HttpServer:
+
+        def __init__(self, host, port):
+            self.host = host
+            self.port = port
+            self.address = (host, port)
+            self._url = '{}://{}:{}'.format(
+                'https' if use_ssl else 'http', host, port)
+
+        def __getitem__(self, key):
+            return properties[key]
+
+        def __setitem__(self, key, value):
+            properties[key] = value
+
+        def url(self, *suffix):
+            return urllib.parse.urljoin(
+                self._url, '/'.join(str(s) for s in suffix))
+
+    class TestHttpServer(tulip.http.ServerHttpProtocol):
+
+        def connection_made(self, transport):
+            transports.append(transport)
+            super().connection_made(transport)
+
+        def handle_request(self, message, payload):
+            if properties.get('close', False):
+                return
+
+            if properties.get('noresponse', False):
+                yield from tulip.sleep(99999)
+
+            if router is not None:
+                body = bytearray()
+                chunk = yield from payload.read()
+                while chunk:
+                    body.extend(chunk)
+                    chunk = yield from payload.read()
+
+                rob = router(
+                    self, properties,
+                    self.transport, message, bytes(body))
+                rob.dispatch()
+
+            else:
+                response = tulip.http.Response(
+                    self.transport, 200, message.version)
+
+                text = b'Test message'
+                response.add_header('Content-type', 'text/plain')
+                response.add_header('Content-length', str(len(text)))
+                response.send_headers()
+                response.write(text)
+                response.write_eof()
+
+    if use_ssl:
+        here = os.path.join(os.path.dirname(__file__), '..', 'tests')
+        keyfile = os.path.join(here, 'sample.key')
+        certfile = os.path.join(here, 'sample.crt')
+        sslcontext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        sslcontext.load_cert_chain(certfile, keyfile)
+    else:
+        sslcontext = None
+
+    def run(loop, fut):
+        thread_loop = tulip.new_event_loop()
+        tulip.set_event_loop(thread_loop)
+
+        socks = thread_loop.run_until_complete(
+            thread_loop.start_serving(
+                lambda: TestHttpServer(keep_alive=0.5),
+                host, port, ssl=sslcontext))
+
+        waiter = tulip.Future(loop=thread_loop)
+        loop.call_soon_threadsafe(
+            fut.set_result, (thread_loop, waiter, socks[0].getsockname()))
+
+        try:
+            thread_loop.run_until_complete(waiter)
+        finally:
+            # call pending connection_made if present
+            run_briefly(thread_loop)
+
+            # close opened trnsports
+            for tr in transports:
+                tr.close()
+
+            run_briefly(thread_loop)  # call close callbacks
+
+            for s in socks:
+                thread_loop.stop_serving(s)
+
+            thread_loop.stop()
+            thread_loop.close()
+            gc.collect()
+
+    fut = tulip.Future(loop=loop)
+    server_thread = threading.Thread(target=run, args=(loop, fut))
+    server_thread.start()
+
+    thread_loop, waiter, addr = loop.run_until_complete(fut)
+    try:
+        yield HttpServer(*addr)
+    finally:
+        thread_loop.call_soon_threadsafe(waiter.set_result, None)
+        server_thread.join()
+
+
+class Router:
+
+    _response_version = "1.1"
+    _responses = http.server.BaseHTTPRequestHandler.responses
+
+    def __init__(self, srv, props, transport, message, payload):
+        # headers
+        self._headers = http.client.HTTPMessage()
+        for hdr, val in message.headers:
+            self._headers.add_header(hdr, val)
+
+        self._srv = srv
+        self._props = props
+        self._transport = transport
+        self._method = message.method
+        self._uri = message.path
+        self._version = message.version
+        self._compression = message.compression
+        self._body = payload
+
+        url = urllib.parse.urlsplit(self._uri)
+        self._path = url.path
+        self._query = url.query
+
+    @staticmethod
+    def define(rmatch):
+        def wrapper(fn):
+            f_locals = sys._getframe(1).f_locals
+            mapping = f_locals.setdefault('_mapping', [])
+            mapping.append((re.compile(rmatch), fn.__name__))
+            return fn
+
+        return wrapper
+
+    def dispatch(self):  # pragma: no cover
+        for route, fn in self._mapping:
+            match = route.match(self._path)
+            if match is not None:
+                try:
+                    return getattr(self, fn)(match)
+                except Exception:
+                    out = io.StringIO()
+                    traceback.print_exc(file=out)
+                    self._response(500, out.getvalue())
+
+                return
+
+        return self._response(self._start_response(404))
+
+    def _start_response(self, code):
+        return tulip.http.Response(self._transport, code)
+
+    def _response(self, response, body=None, headers=None, chunked=False):
+        r_headers = {}
+        for key, val in self._headers.items():
+            key = '-'.join(p.capitalize() for p in key.split('-'))
+            r_headers[key] = val
+
+        encoding = self._headers.get('content-encoding', '').lower()
+        if 'gzip' in encoding:  # pragma: no cover
+            cmod = 'gzip'
+        elif 'deflate' in encoding:
+            cmod = 'deflate'
+        else:
+            cmod = ''
+
+        resp = {
+            'method': self._method,
+            'version': '%s.%s' % self._version,
+            'path': self._uri,
+            'headers': r_headers,
+            'origin': self._transport.get_extra_info('addr', ' ')[0],
+            'query': self._query,
+            'form': {},
+            'compression': cmod,
+            'multipart-data': []
+        }
+        if body:  # pragma: no cover
+            resp['content'] = body
+
+        ct = self._headers.get('content-type', '').lower()
+
+        # application/x-www-form-urlencoded
+        if ct == 'application/x-www-form-urlencoded':
+            resp['form'] = urllib.parse.parse_qs(self._body.decode('latin1'))
+
+        # multipart/form-data
+        elif ct.startswith('multipart/form-data'):  # pragma: no cover
+            out = io.BytesIO()
+            for key, val in self._headers.items():
+                out.write(bytes('{}: {}\r\n'.format(key, val), 'latin1'))
+
+            out.write(b'\r\n')
+            out.write(self._body)
+            out.write(b'\r\n')
+            out.seek(0)
+
+            message = email.parser.BytesParser().parse(out)
+            if message.is_multipart():
+                for msg in message.get_payload():
+                    if msg.is_multipart():
+                        logging.warn('multipart msg is not expected')
+                    else:
+                        key, params = cgi.parse_header(
+                            msg.get('content-disposition', ''))
+                        params['data'] = msg.get_payload()
+                        params['content-type'] = msg.get_content_type()
+                        resp['multipart-data'].append(params)
+
+        body = json.dumps(resp, indent=4, sort_keys=True)
+
+        # default headers
+        hdrs = [('Connection', 'close'),
+                ('Content-Type', 'application/json')]
+        if chunked:
+            hdrs.append(('Transfer-Encoding', 'chunked'))
+        else:
+            hdrs.append(('Content-Length', str(len(body))))
+
+        # extra headers
+        if headers:
+            hdrs.extend(headers.items())
+
+        if chunked:
+            response.force_chunked()
+
+        # headers
+        response.add_headers(*hdrs)
+        response.send_headers()
+
+        # write payload
+        response.write(client.str_to_bytes(body))
+        response.write_eof()
+
+        # keep-alive
+        if response.keep_alive():
+            self._srv.keep_alive(True)
+
+
+def make_test_protocol(base):
+    dct = {}
+    for name in dir(base):
+        if name.startswith('__') and name.endswith('__'):
+            # skip magic names
+            continue
+        dct[name] = unittest.mock.Mock(return_value=None)
+    return type('TestProtocol', (base,) + base.__bases__, dct)()
+
+
+class TestSelector(selectors.BaseSelector):
+
+    def select(self, timeout):
+        return []
+
+
+class TestLoop(base_events.BaseEventLoop):
+    """Loop for unittests.
+
+    It manages self time directly.
+    If something scheduled to be executed later then
+    on next loop iteration after all ready handlers done
+    generator passed to __init__ is calling.
+
+    Generator should be like this:
+
+        def gen():
+            ...
+            when = yield ...
+            ... = yield time_advance
+
+    Value retuned by yield is absolute time of next scheduled handler.
+    Value passed to yield is time advance to move loop's time forward.
+    """
+
+    def __init__(self, gen=None):
+        super().__init__()
+
+        if gen is None:
+            self._check_on_close = False
+            def gen():
+                yield
+        else:
+            self._check_on_close = True
+
+        self._gen = gen()
+        next(self._gen)
+        self._time = 0
+        self._timers = []
+        self._selector = TestSelector()
+
+        self.readers = {}
+        self.writers = {}
+        self.reset_counters()
+
+    def time(self):
+        return self._time
+
+    def advance_time(self, advance):
+        """Move test time forward."""
+        if advance:
+            self._time += advance
+
+    def close(self):
+        if self._check_on_close:
+            try:
+                self._gen.send(0)
+            except StopIteration:
+                pass
+            else:  # pragma: no cover
+                raise AssertionError("Time generator is not finished")
+
+    def add_reader(self, fd, callback, *args):
+        self.readers[fd] = events.make_handle(callback, args)
+
+    def remove_reader(self, fd):
+        self.remove_reader_count[fd] += 1
+        if fd in self.readers:
+            del self.readers[fd]
+            return True
+        else:
+            return False
+
+    def assert_reader(self, fd, callback, *args):
+        assert fd in self.readers, 'fd {} is not registered'.format(fd)
+        handle = self.readers[fd]
+        assert handle._callback == callback, '{!r} != {!r}'.format(
+            handle._callback, callback)
+        assert handle._args == args, '{!r} != {!r}'.format(
+            handle._args, args)
+
+    def add_writer(self, fd, callback, *args):
+        self.writers[fd] = events.make_handle(callback, args)
+
+    def remove_writer(self, fd):
+        self.remove_writer_count[fd] += 1
+        if fd in self.writers:
+            del self.writers[fd]
+            return True
+        else:
+            return False
+
+    def assert_writer(self, fd, callback, *args):
+        assert fd in self.writers, 'fd {} is not registered'.format(fd)
+        handle = self.writers[fd]
+        assert handle._callback == callback, '{!r} != {!r}'.format(
+            handle._callback, callback)
+        assert handle._args == args, '{!r} != {!r}'.format(
+            handle._args, args)
+
+    def reset_counters(self):
+        self.remove_reader_count = collections.defaultdict(int)
+        self.remove_writer_count = collections.defaultdict(int)
+
+    def _run_once(self):
+        super()._run_once()
+        for when in self._timers:
+            advance = self._gen.send(when)
+            self.advance_time(advance)
+        self._timers = []
+
+    def call_at(self, when, callback, *args):
+        self._timers.append(when)
+        return super().call_at(when, callback, *args)
+
+    def _process_events(self, event_list):
+        return
+
+    def _write_to_self(self):
+        pass

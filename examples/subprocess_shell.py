8d8382ab1ab02a6a12d2e85a7047102b952e9a14
Victor Stinner <victor.stinner@gmail.com>
2014-01-28 02:09:13 +0100

    Rewrite subprocess_shell example to use the subprocess_stream module

--- examples/subprocess_shell.py
+++ examples/subprocess_shell.py
@@ -4,67 +4,6 @@ import asyncio
 import subprocess
 
 
-class MyProtocol(asyncio.streams.FlowControlMixin, asyncio.SubprocessProtocol):
-    """Like StreamReaderProtocol, but for a subprocess."""
-
-    def __init__(self):
-        super().__init__()
-        self.loop = asyncio.get_event_loop()
-        self.stdin = self.stdout = self.stderr = None
-        self.waiter = asyncio.Future()
-
-    def connection_made(self, transport):
-        if transport.get_pipe_transport(1):
-            self.stdout = asyncio.StreamReader()
-        if transport.get_pipe_transport(2):
-            self.stderr = asyncio.StreamReader()
-        if  transport.get_pipe_transport(0):
-            self.stdin = asyncio.StreamWriter(transport=transport.get_pipe_transport(0),
-                                              protocol=self,  # Must have _paused, _drain_waiter
-                                              reader=self.stdout,
-                                              loop=self.loop)
-        self.waiter.set_result(None)
-
-    def pipe_data_received(self, fd, data):
-        if fd == 1:
-            reader = self.stdout
-        elif fd == 2:
-            reader = self.stderr
-        else:
-            reader = None
-        if reader is not None:
-            reader.feed_data(data)
-
-    def pipe_connection_lost(self, fd, exc):
-        if fd == 0:
-            pipe = self.stdin
-            self.stdin = None
-            if pipe is not None:
-                pipe.close()
-            return
-        if fd == 1:
-            reader = self.stdout
-            self.stdout = None
-        elif fd == 2:
-            reader = self.stderr
-            self.stderr = None
-        else:
-            reader = None
-        if reader != None:
-            if exc is None:
-                reader.feed_eof()
-            else:
-                reader.set_exception(exc)
-
-
-@asyncio.coroutine
-def shell(cmd, **kwds):
-    loop = asyncio.get_event_loop()
-    transport, protocol = yield from loop.subprocess_shell(MyProtocol, cmd, **kwds)
-    yield from protocol.waiter
-    return protocol.stdin, protocol.stdout, protocol.stderr
-
-
 @asyncio.coroutine
 def send_input(writer, input):
     for line in input:
@@ -77,7 +16,6 @@ def send_input(writer, input):
             print('resume writing')
     writer.close()
 
-
 @asyncio.coroutine
 def log_errors(reader):
     while True:
@@ -86,28 +24,38 @@ def log_errors(reader):
             break
         print('ERROR', repr(line))
 
+@asyncio.coroutine
+def read_stdout(stdout):
+    while True:
+        line = yield from stdout.readline()
+        print('received', repr(line))
+        if not line:
+            break
 
 @asyncio.coroutine
 def start(cmd, input=None, **kwds):
     if input is None and 'stdin' not in kwds:
         kwds['stdin'] = None
-    stdin, stdout, stderr = yield from shell(cmd, **kwds)
+    transport, proc = yield from asyncio.subprocess_shell(cmd, **kwds)
+
+    tasks = []
     if input is not None:
-        asyncio.async(send_input(stdin, input))
+        tasks.append(send_input(proc.stdin, input))
     else:
         print('No stdin')
-    if stderr is not None:
-        asyncio.async(log_errors(stderr))
+    if proc.stderr is not None:
+        tasks.append(log_errors(proc.stderr))
     else:
         print('No stderr')
-    if stdout is not None:
-        while True:
-            line = yield from stdout.readline()
-            print('received', repr(line))
-            if not line:
-                break
+    if proc.stdout is not None:
+        tasks.append(read_stdout(proc.stdout))
     else:
         print('No stdout')
+    # feed stdin while consuming stdout to avoid hang when stdin pipe is full
+    yield from asyncio.wait(tasks)
+
+    exitcode = yield from proc.wait()
+    print("exit code: %s" % exitcode)
 
 
 def main():

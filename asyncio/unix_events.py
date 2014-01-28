a73eea9c3c1aaa5400c54baeb86906d599b26cc1
Victor Stinner <victor.stinner@gmail.com>
2014-01-28 02:09:42 +0100

    _fatal_error() of _UnixWritePipeTransport and _ProactorBasePipeTransport don't log BrokenPipeError nor ConnectionResetError
    Same behaviour than _SelectorTransport._fatal_error()

--- asyncio/unix_events.py
+++ asyncio/unix_events.py
@@ -371,7 +371,8 @@ class _UnixWritePipeTransport(selector_events._FlowControlMixin,
 
     def _fatal_error(self, exc):
         # should be called by exception handler only
-        logger.exception('Fatal error for %s', self)
+        if not isinstance(exc, (BrokenPipeError, ConnectionResetError)):
+            logger.exception('Fatal error for %s', self)
         self._close(exc)
 
     def _close(self, exc=None):

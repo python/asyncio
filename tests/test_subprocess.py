b6feede92ca32182d167abcdd8e8ee7713d8764d
Victor Stinner <victor.stinner@gmail.com>
2014-01-29 18:22:02 +0100

    Add a first unit test for subprocess

---
+++ tests/test_subprocess.py
@@ -0,0 +1,27 @@
+import asyncio
+import asyncio.subprocess
+import os
+import sys
+import unittest
+
+class SubprocessTestCase(unittest.TestCase):
+    def setUp(self):
+        policy = asyncio.get_event_loop_policy()
+        if os.name == 'nt':
+            self.loop = asyncio.ProactorEventLoop()
+        else:
+            self.loop = policy.new_event_loop()
+
+    def test_shell(self):
+        args = [sys.executable, '-c', 'pass']
+
+        @asyncio.coroutine
+        def run():
+            return (yield from asyncio.subprocess.call(*args, loop=self.loop))
+
+        exitcode = self.loop.run_until_complete(run())
+        self.assertEqual(exitcode, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()

e81724d15d78ac3aa9ba067fe8639963c0b9fadb
Victor Stinner <victor.stinner@gmail.com>
2014-07-03 17:24:21 +0200

    Backed out changeset b288da71fb40
    Oops, I wanted to send this patch for review before

--- asyncio/streams.py
+++ asyncio/streams.py
@@ -213,7 +213,7 @@ class StreamReaderProtocol(FlowControlMixin, protocols.Protocol):
             res = self._client_connected_cb(self._stream_reader,
                                             self._stream_writer)
             if coroutines.iscoroutine(res):
-                tasks.task_factory(res, loop=self._loop)
+                tasks.Task(res, loop=self._loop)
 
     def connection_lost(self, exc):
         if exc is None:

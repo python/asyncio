24a1f6152862b2ab4c671b7ba6325e3e99c35b1d
Victor Stinner <victor.stinner@gmail.com>
2014-07-25 18:33:33 +0200

    Tulip issue #196: IocpProactor._poll() clears the reference to the overlapped operation when the operation is done.
    It would be better to clear the reference in a new
    _OverlappedFuture.set_result() method, but it cannot be done yet because of a
    weird bug.

--- asyncio/windows_events.py
+++ asyncio/windows_events.py
@@ -446,6 +446,11 @@ class IocpProactor:
                 f.set_exception(e)
             else:
                 f.set_result(value)
+                # FIXME, tulip issue #196: add _OverlappedFuture.set_result()
+                # method to clear the refrence, don't do it here (f may
+                # by a _WaitHandleFuture). Problem: clearing the reference
+                # in _register() if ov.pedding is False leads to weird bugs.
+                f._ov = None
         return f
 
     def _get_accept_socket(self, family):

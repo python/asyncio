61199a49a0782b1cf83f7dc32719b456d950ba3f
Victor Stinner <vstinner@redhat.com>
2015-07-11 11:48:22 +0200

    enhance git merge

--- update-asyncio-step1.sh
+++ update-asyncio-step1.sh
@@ -3,6 +3,10 @@ git checkout trollius
 git pull -u
 git checkout master
 git pull https://github.com/python/asyncio.git
+
 git checkout trollius
-git merge master
+# rename-threshold=25: a similarity of 25% is enough to consider two files
+# rename candidates
+git merge -X rename-threshold=25 master
+
 echo "Now run ./update-tulip-step2.sh"

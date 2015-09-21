0fccd53fa1fca5eea1d357bbc3e0d1e82679307d
Victor Stinner <vstinner@redhat.com>
2015-09-21 22:31:13 +0200

    update_asyncio.sh: more verbose

--- update_asyncio.sh
+++ update_asyncio.sh
@@ -1,2 +1,12 @@
-cp ~/prog/python/default/Lib/asyncio/*.py asyncio/
-cp ~/prog/python/default/Lib/test/test_asyncio/test_*.py tests/
+PYTHON=~/prog/python/default
+ASYNCIO=.
+
+echo "Sync from $PYTHON to $ASYNCIO"
+set -e -x
+echo
+
+cp $PYTHON/Lib/asyncio/*.py asyncio/
+cp $PYTHON/Lib/test/test_asyncio/test_*.py tests/
+echo
+
+git status

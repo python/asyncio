86245f9e5d28466e7e39c81d62d3ba71ccfb77d9
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-10-02 18:35:42 +0200

    revert Future._copy_state

--- asyncio/futures.py
+++ asyncio/futures.py
@@ -358,6 +358,26 @@ class Future:
             # have had a chance to call result() or exception().
             self._loop.call_soon(self._tb_logger.activate)
 
+    # Truly internal methods.
+
+    def _copy_state(self, other):
+        """Internal helper to copy state from another Future.
+        The other Future may be a concurrent.futures.Future.
+        """
+        assert other.done()
+        if self.cancelled():
+            return
+        assert not self.done()
+        if other.cancelled():
+            self.cancel()
+        else:
+            exception = other.exception()
+            if exception is not None:
+                self.set_exception(exception)
+            else:
+                result = other.result()
+                self.set_result(result)
+
     def __iter__(self):
         if not self.done():
             self._blocking = True
